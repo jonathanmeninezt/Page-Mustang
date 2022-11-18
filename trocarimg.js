7543378a9f644cb57d3c67ff24259028d4eb7c9c
Jonathan Meninez <jonathan.meninezt@gmail.com>
2022-11-17 21:26:34 -0300

    Page Mustang Responsive Option colors

---
+++ trocarimg.js
@@ -0,0 +1,34 @@
+function imgdourado(){
+    document.getElementById("trocarimg").src="img/carrodourado.jpg"
+}
+function imgpreto (){
+	document.getElementById("trocarimg").src="img/carropreto.jpg"
+}
+
+function imgcinzver (){
+	document.getElementById("trocarimg").src="img/carrocinzver.jpg"
+}
+
+function imgcinza (){
+	document.getElementById("trocarimg").src="img/carrocinza.jpg"
+}
+
+function imgazul (){
+	document.getElementById("trocarimg").src="img/carroazul.jpg"
+}
+
+function imgroxo (){
+	document.getElementById("trocarimg").src="img/carroroxo.jpg"
+}
+
+function imgbranco (){
+	document.getElementById("trocarimg").src="img/carrobranco.jpg"
+}
+
+function imgverde (){
+	document.getElementById("trocarimg").src="img/carroverde.jpg"
+}
+
+function imgvermelho (){
+	document.getElementById("trocarimg").src="img/carrovermelho.jpg"
+}
